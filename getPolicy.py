b4f14d19ff06695745972c432b89a5cb36596cca
tjobarow <tjobarow@gmail.com>
2021-08-18 19:11:08 -0400

    Working on pulling authz profile and dACL info for only used dACLs

--- getPolicy.py
+++ getPolicy.py
@@ -6,17 +6,35 @@ import os
 
 def main():
     api = IdentityServicesEngineAPI(username="ers_admin",password="Cisco123!",version="3.0.0",base_url='https://ise31.obarowski.lab',verify=False)
+    
+    policy_set_rules = {}
+
     policy_sets = getPolicySets(api)
-    policy_set_rules = getNACRules(policy_sets,api)
-    os.remove("output.json")
-    output = open("output.json","a+")
-    output.write(json.dumps(policy_set_rules, indent=4, sort_keys=True))
+    policy_set_rules.update({"policy_set_details":policy_sets})
+
+    policy_set_rules.update({"all_policy_set_rules":getNACRules(policy_sets,api)})
+    
+    policy_set_rules.update({"authorization_profiles":getAuthzProfiles(api)})
+
+    policy_set_rules.update({"dacls":getDownloadableACL(api)})
+
+    listAuthzDaclInUse(policy_set_rules,api)
+    
+    try:
+        os.remove("output.json")
+    except:
+        print("File not found, creating it.")
+    finally:
+        output = open("output.json","a+")
+        output.write(json.dumps(policy_set_rules, indent=4, sort_keys=True))
 
 def getPolicySets(api):
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GETTING POLICY SETS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     policy_sets = api.network_access_policy_set.get_network_access_policy_sets().response['response']
     return policy_sets
 
 def getNACRules(policy_sets,api):
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GETTING NAC RULES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     ### authc data structures
     authc_by_policy_set = {}
     authc_rule_structure = {}
@@ -43,17 +61,11 @@ def getNACRules(policy_sets,api):
             authz_loc_ex_rules = api.network_access_authorization_exception_rules.get_network_access_local_exception_rules(policy_id=policy_set['id']).response['response']
         except:
             print("Try: No local exceptions exist for policy set {}".format(policy_set['name']))
-        #Additionally, if the request returns object with length 0. 
-        if len(authz_loc_ex_rules) == 0:
-            print("No local exceptions exist for policy set {}".format(policy_set['name']))
-
+        #API call for global policy authz exceptions. Try to make call, and if it throws an exception, they dont exist
         try:
             authz_glo_ex_rules = api.network_access_authorization_global_exception_rules.get_network_access_policy_set_global_exception_rules().response['response']
         except:
             print("Try: No global exceptions exist for policy set {}".format(policy_set['name']))
-        
-        if len(authz_glo_ex_rules) == 0:
-            print("No global exceptions exist for policy set {}".format(policy_set['name']))
 
         authz_rules = api.network_access_authorization_rules.get_network_access_authorization_rules(policy_id=policy_set['id']).response['response']
 
@@ -85,44 +97,58 @@ def getNACRules(policy_sets,api):
 
     return authc_by_policy_set
 
-def getLocalExAuthzRules(policy_sets,policy_set_rules,api):
-    authz_rule_structure = {}
-    authz_holder = {}
-    for policy_set in policy_sets:
-        authz_rules = api.network_access_authorization_exception_rules.get_network_access_local_exception_rules(policy_id=policy_set['id']).response['response']
-        for authz_rule in authz_rules:
-            authz_rule_structure.update({authz_rule['rule']['id'] : authz_rules.copy()})
-        authz_holder.update({ "local_exception_authorization_rules":authz_rule_structure.copy()})
-        policy_set_rules.update({policy_set['id']:authz_holder.copy()})
-        authz_holder.clear()
-        authz_rule_structure.clear()
-    return policy_set_rules
+def getAuthzProfiles(api):
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GETTING AUTHZ PROFILES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    authz_profiles = api.authorization_profile.get_authorization_profiles().response['SearchResult']['resources']
 
-def getGlobalExAuthzRules(policy_sets,policy_set_rules,api):
-    authz_rule_structure = {}
-    authz_holder = {}
-    for policy_set in policy_sets:
-        authz_rules = api.network_access_authorization_global_exception_rules.get_network_access_policy_set_global_exception_rules()(policy_id=policy_set['id']).response['response']
-        for authz_rule in authz_rules:
-            authz_rule_structure.update({authz_rule['rule']['id'] : authz_rules.copy()})
-        authz_holder.update({ "local_exception_authorization_rules":authz_rule_structure.copy()})
-        policy_set_rules.update({policy_set['id']:authz_holder.copy()})
-        authz_holder.clear()
-        authz_rule_structure.clear()
-    return policy_set_rules
+    authz_profile_obj = {}
 
-def getNormAuthzRules(policy_sets,policy_set_rules,api):
-    authz_rule_structure = {}
-    authz_holder = {}
-    for policy_set in policy_sets:
-        authz_rules = api.network_access_authorization_rules.get_network_access_authorization_rules(policy_id=policy_set['id']).response['response']
-        for authz_rule in authz_rules:
-            authz_rule_structure.update({authz_rule['rule']['id'] : authz_rules.copy()})
-        authz_holder.update({ "authorization_rules":authz_rule_structure.copy()})
-        policy_set_rules.update({policy_set['id']:authz_holder.copy()})
-        authz_holder.clear()
-        authz_rule_structure.clear()
-    return policy_set_rules
+    for authz_profile in authz_profiles:
+        authz_profile_obj.update({authz_profile['name']:authz_profile})
+
+    return authz_profile_obj
+
+def getDownloadableACL(api):
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GETTING DACLS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    dacls = api.downloadable_acl.get_downloadable_acl().response['SearchResult']['resources']
+    dacl_obj = {}
+    for dacl in dacls:
+        dacl_contents = api.downloadable_acl.get_downloadable_acl_by_id(id=dacl['id']).response
+        dacl_obj.update({dacl_contents['DownloadableAcl']['name']:dacl_contents['DownloadableAcl']})
+        #print(json.dumps(dacl_contents, indent=4, sort_keys=True))
+    return dacl_obj
+
+def listAuthzDaclInUse(policy_set_rules,api):
+    authzInUse = []
+    authzInUseNames = []
+    
+    #print(json.dumps(policy_set_rules['all_policy_set_rules'], indent=4, sort_keys=True))
+
+    for policy_set in policy_set_rules['all_policy_set_rules']:
+
+        #print(json.dumps(policy_set, indent=4, sort_keys=True))
+
+        for authz_rule in policy_set_rules['all_policy_set_rules'][policy_set][3]:
+            rule_contents = policy_set_rules['all_policy_set_rules'][policy_set][3][authz_rule]
+            #print(json.dumps(rule_contents, indent=4, sort_keys=True))
+            authzInUse.append({
+                "profileName":rule_contents['profile'][0]
+                ,"usedInAuthzRule":rule_contents['rule']['name']
+                ,"usedInPolicySet":policy_set
+                }.copy())
+            authzInUseNames.append(str(rule_contents['profile'][0]))
+
+    #### IF THE AUTHZ PROFILE IS IN THE LIST OF USED PROFILES, WE GRAB THE AUTHZ PROFILE INFO
+    authzInUseNames = list(dict.fromkeys(authzInUseNames))
+    for authz_prof_name in authzInUseNames:
+        authz_profile = api.authorization_profile.get_authorization_profile_by_name(name=authz_prof_name).response
+        
+
+            
+
+
+
+
 
 if __name__ == '__main__':
     urllib3.disable_warnings()
